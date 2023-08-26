ec387b88ec35e62321382cebe9dd0f68bf5b06d5
Ashikms1998 <142452260+Ashikms1998@users.noreply.github.com>
2023-08-26 08:45:59 +0530

    final commit

---
+++ assets/js/submit.js
@@ -0,0 +1,10 @@
+
+const submit=document.querySelector(".text-center");
+
+
+const text="Loading....\n Your message has been sent. Thank you! "
+
+
+submit.addEventListener("click",()=>{
+    alert(text)
+})
